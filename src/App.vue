53e766ea6f32a5ec7bfa732e847ff217b3f96ba3
1NSiGHT <kent.olayvar117@gmail.com>
2023-08-04 13:11:35 -0300

    No changes

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div class="container">
     <Header title="Task Tracker" />
   </div>
+  
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+
 export default {
   name: "App",
   components: {
