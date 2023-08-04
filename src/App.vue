d9e87d0bea8ae92a2452a77db8ace87f27689f2a
1NSiGHT <kent.olayvar117@gmail.com>
2023-08-04 15:28:33 -0300

    Part of same changes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     };
   },
-  create() {
+  created() {
     this.tasks = [
       {
         id: 1,
