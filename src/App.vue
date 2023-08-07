8fa7e3f30a2425d031483c135d2d175f2e44fecd
1NSiGHT <kent.olayvar117@gmail.com>
2023-08-07 13:06:11 -0300

    Added a footer and implemented routes (not done)

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,13 @@
       @delete-task="deleteTask"
       :tasks="tasks"
     />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 import Tasks from "./components/Tasks.vue";
 import AddTask from "./components/AddTask.vue";
 
@@ -25,6 +27,7 @@ export default {
   name: "App",
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask,
   },
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+        <p> Copyright &copy; 2023 </p>
+        <a href="/about">About</a>
+    </footer>
+</template>
+
+
+<style scoped> 
+    a {
+        color: #42b983;
+    }
+
+
+    footer {
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
